b4d4c794c438695e0739b24beb2cf9f0a049ca57
Seamooo <48548685+Seamooo@users.noreply.github.com>
2022-10-31 21:14:02 +0800

    fixed off by one for diagnostic generation, added test to check for it (#20)

--- ruffd-core/src/server_ops.rs
+++ ruffd-core/src/server_ops.rs
@@ -7,6 +7,7 @@ use ruffd_types::{
     CreateLocksFn, RpcNotification, RwGuarded, RwReq, ScheduledTask, ServerNotification,
     ServerNotificationExec, ServerState, ServerStateHandles, ServerStateLocks,
 };
+use std::path::Path;
 use std::sync::Arc;
 
 // TODO macro the create locks fn
@@ -17,10 +18,10 @@ fn message_into_diagnostic(msg: Message) -> lsp_types::Diagnostic {
     // only have it span a single character
     let range = {
         // diagnostic is zero indexed, but message rows are 1-indexed
-        let row_start = msg.location.row() as u32;
-        let col_start = msg.location.column() as u32 - 1;
-        let row_end = msg.end_location.row() as u32;
-        let col_end = msg.end_location.column() as u32 - 1;
+        let row_start = msg.location.row() as u32 - 1;
+        let col_start = msg.location.column() as u32;
+        let row_end = msg.end_location.row() as u32 - 1;
+        let col_end = msg.end_location.column() as u32;
         let start = lsp_types::Position {
             line: row_start,
             character: col_start,
@@ -48,6 +49,14 @@ fn message_into_diagnostic(msg: Message) -> lsp_types::Diagnostic {
     }
 }
 
+fn diagnostics_from_doc(path: &Path, doc: &str) -> Vec<lsp_types::Diagnostic> {
+    check(path, doc)
+        .unwrap_or_default()
+        .into_iter()
+        .map(message_into_diagnostic)
+        .collect()
+}
+
 // NOTE require interface from ruff before checks can be run
 pub fn run_diagnostic_op(document_uri: lsp_types::Url) -> ServerNotification {
     let exec: ServerNotificationExec = Box::new(
@@ -57,11 +66,11 @@ pub fn run_diagnostic_op(document_uri: lsp_types::Url) -> ServerNotification {
                     RwGuarded::Read(x) => x,
                     _ => unreachable!(),
                 };
-                let messages: Vec<Message> = {
+                let diagnostics = {
                     if let Some(buffer) = open_buffers.get(&document_uri) {
                         let doc = buffer.iter().collect::<String>();
                         if let Ok(path) = document_uri.to_file_path() {
-                            check(&path, &doc).unwrap_or_default()
+                            diagnostics_from_doc(&path, doc.as_str())
                         } else {
                             vec![]
                         }
@@ -69,10 +78,6 @@ pub fn run_diagnostic_op(document_uri: lsp_types::Url) -> ServerNotification {
                         vec![]
                     }
                 };
-                let diagnostics = messages
-                    .into_iter()
-                    .map(message_into_diagnostic)
-                    .collect::<Vec<_>>();
                 RpcNotification::new(
                     "textDocument/publishDiagnostics".to_string(),
                     Some(
@@ -99,4 +104,35 @@ pub fn run_diagnostic_op(document_uri: lsp_types::Url) -> ServerNotification {
         })
     });
     ServerNotification { exec, create_locks }
+}
+
+#[cfg(test)]
+mod test {
+    use super::*;
+
+    #[test]
+    fn test_diagnostic_gen_position() {
+        let doc = r#"
+def bar():
+    x = 0
+    print('does this now work?')
+"#;
+        let path = lsp_types::Url::parse("file:///tmp/dummy.py")
+            .unwrap()
+            .to_file_path()
+            .unwrap();
+        let diagnostics = diagnostics_from_doc(&path, doc);
+        let expected_range = lsp_types::Range {
+            start: lsp_types::Position {
+                line: 2,
+                character: 4,
+            },
+            end: lsp_types::Position {
+                line: 2,
+                character: 5,
+            },
+        };
+        assert_eq!(diagnostics.len(), 1);
+        assert_eq!(diagnostics[0].range, expected_range);
+    }
 }
